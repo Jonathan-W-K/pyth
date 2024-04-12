d9a650ecbb4f0b07f1d690cf2bf61e45a2489224
Jonathan-W-K <jonagil0987@gmail.com>
2024-04-12 09:33:53 +0300

    latest update atleast as far as i have gone for now

--- registration_me/views.py
+++ registration_me/views.py
@@ -1,7 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-from django.shortcuts import render
-from django.http import HttpResponse
 
 def register(request):
     if request.method == 'POST':
@@ -11,3 +10,14 @@ def register(request):
         # Render registration form template
         return render(request, 'registration/register.html')
 
+def login(request):
+    if request.method == 'POST':
+        username = request
+        password =""
+        return HttpResponse("Login successful!")
+    else:
+        return render(request, 'login/login.html')
+
+def home(request):
+    if request.method == 'POST':
+        return "click here" <a href="home.html">here</a>
